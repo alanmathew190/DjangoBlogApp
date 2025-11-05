66c7ab87466649de39ae20ce32c68a6f7f9069a8
Alan <alanmathew190@gmail.com>
2025-11-05 12:20:49 +0530

    push data post

--- blogLearning/blog1/models.py
+++ blogLearning/blog1/models.py
@@ -7,4 +7,4 @@ class blogInfo(models.Model):
     content=models.TextField()
     
 def __str__(self):
-    return self.title
+    return self.title 
--- blogLearning/blog1/views.py
+++ blogLearning/blog1/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import blogInfo
 
 # Create your views here.
+
+def create(request):
+    if request.method=='POST':
+        title=request.POST.get('title')
+        content=request.POST.get('content')
+        blogObj=blogInfo(title=title,content=content)
+        blogObj.save()
+    return render(request,'create.html')
+
+def list(request):
+    return render(request,'list.html')
