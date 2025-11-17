c16b7b091397a548e020aa6af3618c2101c5032b
Alan <alanmathew190@gmail.com>
2025-11-17 19:14:06 +0530

    added postgres database

--- blogLearning/blogLearning/settings.py
+++ blogLearning/blogLearning/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog1',
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'blogLearning.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogapp',
+        'USER':'postgres',
+        'PASSWORD':'alan123',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
